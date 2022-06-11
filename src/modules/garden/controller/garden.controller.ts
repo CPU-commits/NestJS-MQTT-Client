4f33657247064a64f6855e8f7810192c77cbe0f0
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-11 05:56:33 -0400

    [update] Controller

--- src/modules/garden/controller/garden.controller.ts
+++ src/modules/garden/controller/garden.controller.ts
@@ -1,14 +1,23 @@
 import { Controller } from '@nestjs/common'
 import { MessagePattern, Payload } from '@nestjs/microservices'
+import { GardenService } from '../service/garden.service'
 
 @Controller('garden')
 export class GardenController {
+    constructor(private gardenService: GardenService) {}
+
     @MessagePattern('garden/message')
     getMessage(@Payload() data: string) {
         let splitData: any = data.split('.')
         splitData = splitData.map((text: string) => {
             return text.split(':')
         })
-        console.log(splitData)
+        this.gardenService.insertMetrics({
+            agent: splitData[0][1],
+            temperature: parseInt(splitData[1][1]),
+            rain: splitData[2][1] === '1',
+            rh: parseInt(splitData[3][1]),
+            day: splitData[4][1] === '1',
+        })
     }
 }
