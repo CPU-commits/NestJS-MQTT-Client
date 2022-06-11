3775929c34d02caf0beae7e3c293d5a60a81dcfb
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-11 05:57:22 -0400

    [update] Service

--- src/modules/mqtt/service/mqtt.service.ts
+++ src/modules/mqtt/service/mqtt.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { Agent } from '../entities/agents.entity'
+import { Agent as AgentModel } from '../models/agent.model'
 
 @Injectable()
 export class MqttService {
-    public readonly agents = []
+    public readonly agents: Array<AgentModel> = []
     private readonly CLIENT_NEST_AGENT = 'Client-001-Client'
 
     constructor(@InjectModel(Agent.name) private agentModel: Model<Agent>) {
         this.createNestAgent()
+    }
+
+    insertAgentArray(agent: AgentModel) {
+        this.agents.push(agent)
     }
 
     private async createNestAgent() {
@@ -17,35 +22,59 @@ export class MqttService {
         if (!nestAgent) this.createAgent(this.CLIENT_NEST_AGENT)
     }
 
-    private async findAgent(agent: string) {
+    async findAgent(agent: string) {
         return await this.agentModel.findOne({
             name: agent,
         })
     }
 
-    private async createAgent(agent: string) {
+    async createAgent(agent: string, connected = true) {
         const splitData = agent.split('-')
+        const now = new Date()
         const newAgent = new this.agentModel({
             name: agent,
             agent_type: splitData[2].toLowerCase(),
             agent_task: splitData[0].toLowerCase(),
+            last_connection: now,
+            created: now,
+            connected,
         })
         return await newAgent.save()
     }
 
     async registerAgent(agent: string) {
-        const agentData = await this.findAgent(agent)
-        if (!this.agents.some((a) => a === agent)) {
-            if (!agentData) {
-                await this.createAgent(agent)
-            }
-            this.agents.push(agent)
+        let agentData = await this.findAgent(agent)
+        if (!this.agents.some((a) => a.agent === agent)) {
+            if (!agentData) agentData = await this.createAgent(agent)
+            this.insertAgentArray({
+                agent,
+                _id: agentData._id.toString(),
+            })
         } else {
             agentData
-                .update(
+                .updateOne(
                     {
                         $set: {
                             connected: true,
+                        },
+                    },
+                    { new: true },
+                )
+                .exec()
+        }
+    }
+
+    async disconnectAgent(agent: string) {
+        const agentData = await this.findAgent(agent)
+        if (!agentData) {
+            this.createAgent(agent, false)
+        } else {
+            return await agentData
+                .updateOne(
+                    {
+                        $set: {
+                            connected: false,
+                            last_connection: new Date(),
                         },
                     },
                     { new: true },
