17de3570f95f019b2e8134f06b6b0ad0a1d12dc5
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-11 04:49:29 -0400

    [update] Validate mongo env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { GardenModule } from './modules/garden/garden.module'
 import { DatabaseModule } from './database/database.module'
-import { MqttModule } from './modules/mqtt/mqtt.module';
+import { MqttModule } from './modules/mqtt/mqtt.module'
 import * as Joi from 'joi'
 // Config
 import config from './config'
@@ -20,11 +20,12 @@ import config from './config'
                 MQTT_HOST: Joi.string().required(),
                 MQTT_USERNAME: Joi.string().required(),
                 MQTT_PASSWORD: Joi.string().required(),
-                PG_USER: Joi.string().required(),
-                PG_PASSWORD: Joi.string().required(),
-                PG_HOST: Joi.string().required(),
-                PG_PORT: Joi.number().required(),
-                PG_DB: Joi.string().required(),
+                MONGO_USER: Joi.string().required(),
+                MONGO_PASSWORD: Joi.string().required(),
+                MONGO_HOST: Joi.string().required(),
+                MONGO_PORT: Joi.number().required(),
+                MONGO_DB: Joi.string().required(),
+                MONGO_CONNECTION: Joi.string().required(),
             }),
         }),
         GardenModule,
