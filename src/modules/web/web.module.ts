c35c8a5ae9fb003e46661c2fe4a781192b57bded
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-16 14:32:49 -0400

    [update] Forward import

--- src/modules/web/web.module.ts
+++ src/modules/web/web.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
+import { GardenModule } from '../garden/garden.module'
+import { MqttModule } from '../mqtt/mqtt.module'
 import { WebController } from './controller/web.controller'
 import { WebService } from './service/web.service'
 import { MetricsGateway } from './websockets/metrics.gateway'
 
 @Module({
+    imports: [forwardRef(() => MqttModule), forwardRef(() => GardenModule)],
     controllers: [WebController],
     providers: [WebService, MetricsGateway],
+    exports: [MetricsGateway],
 })
 export class WebModule {}
