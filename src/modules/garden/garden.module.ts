2276e9fd082f9f2d8d139fa23a5104ac6f339ebb
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-16 14:31:05 -0400

    [update] plant entity

--- src/modules/garden/garden.module.ts
+++ src/modules/garden/garden.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { MqttModule } from '../mqtt/mqtt.module'
+import { WebModule } from '../web/web.module'
 import { GardenController } from './controller/garden.controller'
 import { Garden, SchemaGarden } from './entities/garden.entity'
+import { Plant, SchemaPlant } from './entities/plant.entity'
 import { GardenService } from './service/garden.service'
 
 @Module({
@@ -12,10 +14,16 @@ import { GardenService } from './service/garden.service'
                 name: Garden.name,
                 schema: SchemaGarden,
             },
+            {
+                name: Plant.name,
+                schema: SchemaPlant,
+            },
         ]),
         MqttModule,
+        WebModule,
     ],
     controllers: [GardenController],
     providers: [GardenService],
+    exports: [GardenService],
 })
 export class GardenModule {}
