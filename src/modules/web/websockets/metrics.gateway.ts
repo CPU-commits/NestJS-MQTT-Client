5da3eaf9f02250d892e7fe8157f7b883a79c42c3
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-16 14:32:05 -0400

    [update] Send metrics and agent status

--- src/modules/web/websockets/metrics.gateway.ts
+++ src/modules/web/websockets/metrics.gateway.ts
@@ -1,9 +1,25 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
+import { Server } from 'socket.io'
+import { GardenMetric } from 'src/modules/garden/models/metric.model'
 
-@WebSocketGateway()
+@WebSocketGateway({
+    cors: {
+        origin: '*',
+    },
+})
 export class MetricsGateway {
-    @SubscribeMessage('message')
-    handleMessage(client: any, payload: any): string {
-        return 'Hello world!'
+    @WebSocketServer()
+    private server: Server
+
+    sendGardenMetrics(metrics: GardenMetric) {
+        this.server.emit('garden/metrics', metrics)
+    }
+
+    sendAgentStatus(agent: string, status: boolean, date?: Date) {
+        this.server.emit('agents/status', {
+            agent,
+            status,
+            date,
+        })
     }
 }
