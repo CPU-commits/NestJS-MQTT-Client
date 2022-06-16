8f18dc57fae8f0924f96e9dc5b65e046dd567386
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-16 14:28:54 -0400

    [update] Import Web Module

--- src/modules/mqtt/mqtt.module.ts
+++ src/modules/mqtt/mqtt.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { WebModule } from '../web/web.module'
 import { MqttController } from './controller/mqtt.controller'
 import { Agent, SchemaAgent } from './entities/agents.entity'
+import { SchemaTask, Task } from './entities/task.entity'
 import { MqttService } from './service/mqtt.service'
 
 @Module({
@@ -11,7 +13,12 @@ import { MqttService } from './service/mqtt.service'
                 name: Agent.name,
                 schema: SchemaAgent,
             },
+            {
+                name: Task.name,
+                schema: SchemaTask,
+            },
         ]),
+        WebModule,
     ],
     controllers: [MqttController],
     providers: [MqttService],
