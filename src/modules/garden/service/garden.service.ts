2d33ea63b1926af06fb4cc0634695516981f4d8b
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-16 14:30:17 -0400

    [update] Get data

--- src/modules/garden/service/garden.service.ts
+++ src/modules/garden/service/garden.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import * as moment from 'moment'
+
 import { Agent } from 'src/modules/mqtt/models/agent.model'
 import { MqttService } from 'src/modules/mqtt/service/mqtt.service'
+import { MetricsGateway } from 'src/modules/web/websockets/metrics.gateway'
 import { Garden } from '../entities/garden.entity'
+import { Plant } from '../entities/plant.entity'
 import { GardenMetric } from '../models/metric.model'
 
 @Injectable()
 export class GardenService {
     constructor(
         @InjectModel(Garden.name) private gardenModel: Model<Garden>,
+        @InjectModel(Plant.name) private plantModel: Model<Plant>,
         private mqttService: MqttService,
+        private metricsGW: MetricsGateway,
     ) {}
 
     private async getAgentId(agent: string): Promise<Agent> {
@@ -38,6 +44,35 @@ export class GardenService {
         return newAgent
     }
 
+    private sendMetrics(metrics: GardenMetric) {
+        this.metricsGW.sendGardenMetrics(metrics)
+    }
+
+    async getMetrics(limit = 100, latest?: boolean, date?: string) {
+        const query = this.gardenModel
+            .find(
+                date
+                    ? {
+                          date: {
+                              $gte: date,
+                          },
+                      }
+                    : null,
+            )
+            .limit(limit)
+        if (latest) query.sort({ date: -1 })
+        return (await query.exec()).sort((a, b) => {
+            return moment(a.date).diff(moment(b.date), 'seconds')
+        })
+    }
+
+    async getPlants() {
+        return await this.plantModel
+            .find()
+            .populate('agent', { name: 1 })
+            .exec()
+    }
+
     async insertMetrics(metric: GardenMetric) {
         const { agent, ...rest } = metric
         const agentId = await this.getAgentId(agent)
@@ -46,6 +81,9 @@ export class GardenService {
             agent: agentId._id,
             date: new Date(),
         })
+        // Send metrics Websocket
+        this.sendMetrics(metric)
+        // Save data DB
         return await newMetric.save()
     }
 }
